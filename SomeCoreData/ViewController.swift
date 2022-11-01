0c26d534efbbd505719c035ced5bccccd390ddab
evgen z <evgen-2208@ya.ru>
2022-11-01 12:37:07 +0400

    Initial Commit

---
+++ SomeCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomeCoreData
+//
+//  Created by Evgen on 01/11/2022.
+//  Copyright © 2022 Evgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
